c53107f94c4a3d8430dd3377c0ea16a2cd23d109
hjh2252173 <145317483+hjh2252173@users.noreply.github.com>
2024-04-09 16:33:41 +0900

    6주차

---
+++ chp7/EvCar.java
@@ -0,0 +1,25 @@
+public class EvCar extends Car{
+	int charge = 100;
+	
+	EvCar() {
+		super(2024, "Tesla", "Model X"); // S,3,X,Y 
+	}
+	
+	int getCharge() {
+		return this.charge;
+	}
+	void charge() {
+		this.charge = 100;
+		System.out.println("Your car is change.");
+	}
+	
+	//Car함수 오버라이딩
+	void drive() {
+		this.charge -= 10;
+		this.km += 100;
+		
+		System.out.println("You drove " + 
+					this.km + "km, and have" +
+					this.tank + "% battery.");
+	}
+}
---
+++ chp7/Ex803.java
@@ -0,0 +1,34 @@
+public class Ex803 {
+
+	public static void main(String[] args) {
+		Car sf = new Car(); // Santa Fe
+		EvCar x = new EvCar(); // Model x
+
+		//Drive Santa Fe
+		sf.printDetails();
+		
+		for (int i = 0; i < 6; i++) {
+			sf.drive();
+		}
+		System.out.println("You have " + sf.getTank() + 
+				"L of oil left.");
+		System.out.println("You have " + x.getCharge() + 
+				"% battery left.");
+		System.out.println("You drove " + sf.getkm() + "km.");
+		
+		//Drive model X
+		x.printDetails();
+		
+		for (int i = 0; i < 20; i++) {
+			if(x.getCharge() <= 10) {
+				x.charge();
+			}
+			x.drive();
+		}
+		System.out.println("You have " + x.getCharge() + "% battery left.");
+		System.out.println("You drove " + sf.getkm() + "km.");
+	}
+	
+
+}
+
